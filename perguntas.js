d97d2aad0f38ec2df405c5278ec063d331fc5176
soldadopa <lost.sky4201@proton.me>
2024-12-03 23:57:46 +0000

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fotografia',
+    'O que é o sensor da câmera',
+    'O sensor da câmera detecta a luz incidente que entra pela lente'
+)
+
+criaCartao(
+    'Fotografia',
+    'Quais as principais formas de controlar a iluminação em uma imagem?',
+    'ISO, velocidade do obturador, abertura do diafragma'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'tchau em ingles é BYE (BAI)'
+)
